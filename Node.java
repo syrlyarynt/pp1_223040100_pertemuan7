6e46f7df8dfe315f7a41a3e4753f0222540fbf16
Syerli Aryanti <125714647+syrlyarynt@users.noreply.github.com>
2024-03-24 21:19:17 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan7;
+
+/**
+ *
+ * @author syrly
+ */
+public class Node {
+    
+    private Matakuliah data;
+    private Node next;
+
+    public Node(Matakuliah data) {
+        this.data = data;
+    }
+
+    public Matakuliah getData() {
+        return data;
+    }
+
+    public void setData(Matakuliah data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
